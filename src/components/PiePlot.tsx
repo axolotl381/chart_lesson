3a0684cf91313f5e558377fceefe1dee83c9f5fa
u-pa1 <ryoo@xd5.so-net.ne.jp>
2023-01-23 15:43:10 +0900

    push

---
+++ src/components/PiePlot.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Pie, Doughnut } from "react-chartjs-2";
+
+const data = {
+  labels: ["WIn", "Mac", "Linux"],
+  datasets: [
+    {
+      data: [90, 7, 3],
+      backgroundColor: ["#4169e1", "#ff1493", "#FFCE56"],
+      hoverBackgroundColor: ["#36A2EB", "#FF6384", "#FFCE56"],
+      borderColor: ["transparent", "transparent", "transparent"],
+    },
+  ],
+};
+
+const options = {
+  maintainAspectRatio: false,
+  responsive: false,
+};
+
+const PiePlot: React.FC = () => {
+  return (
+    <div>
+      <Pie data={data} options={options} width={200} height={200}></Pie>
+      <Doughnut data={data}></Doughnut>
+    </div>
+  );
+};
+
+export default PiePlot;
